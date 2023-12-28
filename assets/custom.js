657aae2d7fd31e5fb633adb6c597ad661d3d283d
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-28 19:12:37 +0600

    Almost Complete

--- assets/custom.js
+++ assets/custom.js
@@ -1,8 +1,9 @@
 let sideBar = document.getElementById("side__bar");
 let main = document.getElementById("main");
-let sideNavs = document.getElementsByClassName("side__nav__items");
 document.getElementById("arrow").addEventListener("click", function () {
   sideBar.classList.add("side__bg");
-  sideBar.style.width = "55px";
+  sideBar.style.width = "80px";
   main.style.paddingLeft = "150px";
+  document.getElementById("side__nav").classList.add("sideIcon");
+
 });
