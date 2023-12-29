1e0dec13a7d5c3bb95dddc883961d43435bb5f78
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-29 11:23:23 +0600

    Almost Complete This Pages

--- assets/custom.js
+++ assets/custom.js
@@ -5,5 +5,22 @@ document.getElementById("arrow").addEventListener("click", function () {
   sideBar.style.width = "80px";
   main.style.paddingLeft = "150px";
   document.getElementById("side__nav").classList.add("sideIcon");
+  this.classList.add("d-none")
+  document.getElementById("hiddenArrow").classList.remove("d-none")
+});
+document.getElementById("hiddenArrow").addEventListener("click", function(){
+    sideBar.classList.remove("side__bg");
+    sideBar.style.width = "fit-content";
+    main.style.paddingLeft = "250px";
+    document.getElementById("side__nav").classList.remove("sideIcon");
+    document.getElementById("arrow").classList.remove("d-none")
+   this.classList.add("d-none");
+})
 
+var swiper = new Swiper(".cardSlider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
 });
