764b28970ae1a5ec0dae9e47e71ded29cabbc891
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-30 00:07:33 +0600

    Fix Some Problem

--- assets/custom.js
+++ assets/custom.js
@@ -2,7 +2,6 @@ let sideBar = document.getElementById("side__bar");
 let main = document.getElementById("main");
 document.getElementById("arrow").addEventListener("click", function () {
   sideBar.classList.add("side__bg");
-  sideBar.style.width = "80px";
   main.style.paddingLeft = "150px";
   document.getElementById("side__nav").classList.add("sideIcon");
   this.classList.add("d-none")
